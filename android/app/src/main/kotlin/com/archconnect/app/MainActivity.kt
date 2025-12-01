0aeb2267583899ad5ce3eff001d79d9d0dd698b4
Slowbro121 <goyalankit021@gmail.com>
2025-12-01 10:23:10 +0530

    fix(android) issues

---
+++ android/app/src/main/kotlin/com/archconnect/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.archconnect.app // <--- MAKE SURE THIS MATCHES YOUR NEW PACKAGE NAME
+
+import io.flutter.embedding.android.FlutterActivity // <--- This fixes the "embedding" error
+
+class MainActivity: FlutterActivity() {
+    // No other code is needed here for standard Flutter setup
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'app.dart'; // Import our new app root
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core
 
-void main() {
+void main() async{
   // We can add more initialization logic here later (like Firebase)
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. Initialize Firebase before running the app
+  await Firebase.initializeApp();
 
   runApp(const ArchConnectApp());
 }
